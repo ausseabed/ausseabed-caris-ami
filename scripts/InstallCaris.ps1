ea6bc23a2488aa3cd8e461517c110b4e626c2020
DaveP <david.penton@ga.gov.au>
2020-10-29 04:49:56 +0000

    NGA-650 Update caris ami to 12.3.9 & fix test

--- scripts/InstallCaris.ps1
+++ scripts/InstallCaris.ps1
@@ -1,8 +1,8 @@
 Write-Host "Downloading Caris"
-aws s3 sync s3://caris-installer C:\Windows\Temp\caris-installer
+aws s3 sync s3://caris-installer/11.3.9 C:\Windows\Temp\caris-installer
 
 Write-Host "Installing Caris"
 Start-Process -FilePath "C:\Windows\Temp\caris-installer\setup.exe" -ArgumentList "/S /v/qn" -PassThru -Wait
 
 Write-Host "Add carisbatch to path"
-[System.Environment]::SetEnvironmentVariable('Path', "$ENV:PATH;'C:\Program Files\CARIS\HIPS and SIPS\11.2\bin'", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('Path', $ENV:PATH + ";C:\Program Files\CARIS\HIPS and SIPS\11.3\bin", [System.EnvironmentVariableTarget]::Machine)
